a976c26138ffc58dd52eef4853ca955d9ddb86e9
SlavaGrach <grachv77@gmail.com>
2022-06-13 23:40:59 +0300

    upgrade search bar

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { FilterWrapper, Form, Input, TripsFilter } from './SearchForm.styled'
+import {
+  FilterWrapper,
+  Form,
+  Input,
+  Select,
+  TripsFilter,
+} from './SearchForm.styled'
 
 const SearchForm = () => {
   return (
@@ -9,6 +15,18 @@ const SearchForm = () => {
           <TripsFilter>
             <Input name="search" type="search" placeholder="search by title" />
           </TripsFilter>
+          <Select name="duration">
+            <option value="">duration</option>
+            <option value="0_x_5">&lt; 5 days</option>
+            <option value="5_x_10">&lt; 10 days</option>
+            <option value="10_x">≥ 10 days</option>
+          </Select>
+          <Select name="level">
+            <option value="">level</option>
+            <option value="easy">easy</option>
+            <option value="moderate">moderate</option>
+            <option value="difficult">difficult</option>
+          </Select>
         </Form>
       </FilterWrapper>
     </>
--- src/components/SearchForm/SearchForm.styled.jsx
+++ src/components/SearchForm/SearchForm.styled.jsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import search from '../../assets/img/search.svg'
+import arrowdown from '../../assets/img/arrowdown.svg'
 
 export const FilterWrapper = styled.section`
   display: flex;
@@ -43,3 +44,20 @@ export const Input = styled.input`
   appearance: none;
 `
 export const LabelSelect = styled.label``
+
+export const Select = styled.select`
+  min-width: 160px;
+  min-height: 45px;
+  padding: 10px 40px 10px 15px;
+  font-size: var(--font-size-300);
+  background-color: var(--color-white);
+  background-color: var(--color-white);
+  background-image: url(${arrowdown});
+  background-repeat: no-repeat;
+  background-position: right 15px top 50%;
+  background-size: 15px;
+  border: none;
+  border-radius: 1px;
+  cursor: pointer;
+  appearance: none;
+`
